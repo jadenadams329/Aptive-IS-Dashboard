a43c750bfcfcf04c823bd3b13fd33dac95297297
Jaden <jadenadams329@gmail.com>
2024-04-07 11:03:00 -0700

    add validation on routes

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -5,10 +5,24 @@ const bcrypt = require("bcryptjs");
 const { setTokenCookie, restoreUser } = require("../../utils/auth");
 const { User } = require("../../db/models");
 
+const { check } = require("express-validator");
+const { handleValidationErrors } = require("../../utils/validation");
+
 const router = express.Router();
 
+const validateLogin = [
+	check('credential')
+	  .exists({ checkFalsy: true })
+	  .notEmpty()
+	  .withMessage('Please provide a valid email or username.'),
+	check('password')
+	  .exists({ checkFalsy: true })
+	  .withMessage('Please provide a password.'),
+	handleValidationErrors
+  ];
+
 // Log in
-router.post("/", async (req, res, next) => {
+router.post("/", validateLogin, async (req, res, next) => {
 	const { credential, password } = req.body;
 
 	const user = await User.unscoped().findOne({
@@ -55,7 +69,7 @@ router.get("/", (req, res) => {
 			email: user.email,
 			firstName: user.firstName,
 			lastName: user.lastName,
-			role: user.role
+			role: user.role,
 		};
 		return res.json({
 			user: safeUser,
--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -4,10 +4,36 @@ const bcrypt = require("bcryptjs");
 const { setTokenCookie, requireAuth } = require("../../utils/auth");
 const { User } = require("../../db/models");
 
+const { check } = require('express-validator');
+const { handleValidationErrors } = require('../../utils/validation');
+
 const router = express.Router();
 
+const validateSignup = [
+	check('email')
+	  .exists({ checkFalsy: true })
+	  .isEmail()
+	  .withMessage('Please provide a valid email.'),
+	check('password')
+	  .exists({ checkFalsy: true })
+	  .isLength({ min: 6 })
+	  .withMessage('Password must be 6 characters or more.'),
+	check('firstName')
+		.exists({checkFalsy: true})
+		.isLength({min: 1})
+		.withMessage('First Name must be 1 character or more'),
+	check('lastName')
+		.exists({checkFalsy: true})
+		.isLength({min: 1})
+		.withMessage('Last Name must be 1 character or more'),
+	check('role')
+		.exists({checkFalsy: true})
+		.withMessage('Role must be assigned'),
+	handleValidationErrors
+  ];
+
 // Sign up
-router.post("/", async (req, res) => {
+router.post("/", validateSignup ,async (req, res) => {
 	const { email, password, firstName, lastName, role } = req.body;
 	const hashedPassword = bcrypt.hashSync(password);
 	const user = await User.create({ email, hashedPassword, firstName, lastName, role });
