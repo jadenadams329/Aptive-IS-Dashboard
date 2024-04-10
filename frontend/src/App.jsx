bb0190c65be3a612b4d274e24799d7f45850a6b0
Jaden <jadenadams329@gmail.com>
2024-04-09 21:05:55 -0700

    frontend starter done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,49 +1,55 @@
-import { useState, useEffect } from 'react';
-import { useDispatch } from 'react-redux';
-import { createBrowserRouter, RouterProvider, Outlet } from 'react-router-dom';
-import LoginFormPage from './components/LoginFormPage/LoginFormPage';
-import SignupFormPage from './components/SignUpFormPage/SignUpFormPage';
-import * as sessionActions from './store/session';
+import { useState, useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
+import LoginFormPage from "./components/LoginFormPage/LoginFormPage";
+import SignupFormPage from "./components/SignUpFormPage/SignUpFormPage";
+import Navigation from "./components/Navigation/Navigation";
+import * as sessionActions from "./store/session";
 
 function Layout() {
-  const dispatch = useDispatch();
-  const [isLoaded, setIsLoaded] = useState(false);
+	const dispatch = useDispatch();
+	const [isLoaded, setIsLoaded] = useState(false);
 
-  useEffect(() => {
-    dispatch(sessionActions.restoreUser()).then(() => {
-      setIsLoaded(true)
-    });
-  }, [dispatch]);
+	useEffect(() => {
+		dispatch(sessionActions.restoreUser()).then(() => {
+			setIsLoaded(true);
+		});
+	}, [dispatch]);
 
-  return (
+	return (
     <>
-      {isLoaded && <Outlet />}
-    </>
-  );
+    <Navigation isLoaded={isLoaded} />
+    {isLoaded && (
+      <div className="main-content"> 
+        <Outlet />
+      </div>
+    )}
+  </>
+	);
 }
 
 const router = createBrowserRouter([
-  {
-    element: <Layout />,
-    children: [
-      {
-        path: '/',
-        element: <h1>Welcome!</h1>
-      },
-      {
-        path: '/login',
-        element: <LoginFormPage />
-      },
-      {
-        path: "/signup",
-        element: <SignupFormPage />
-      }
-    ]
-  }
+	{
+		element: <Layout />,
+		children: [
+			{
+				path: "/",
+				element: <h1>Welcome!</h1>,
+			},
+			{
+				path: "/login",
+				element: <LoginFormPage />,
+			},
+			{
+				path: "/signup",
+				element: <SignupFormPage />,
+			},
+		],
+	},
 ]);
 
 function App() {
-  return <RouterProvider router={router} />;
+	return <RouterProvider router={router} />;
 }
 
 export default App;
