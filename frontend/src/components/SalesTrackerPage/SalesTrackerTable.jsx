d9998bc3b4c947032e50cd90c1e705bb5a3112e0
Jaden <jadenadams329@gmail.com>
2024-12-02 11:21:32 -0800

    more progress

--- frontend/src/components/SalesTrackerPage/SalesTrackerTable.jsx
+++ frontend/src/components/SalesTrackerPage/SalesTrackerTable.jsx
@@ -5,10 +5,11 @@ import Spinner from "../Spinner/Spinner";
 import { deleteSale, getUserSales } from "../../store/userSales";
 import OpenModalButton from "../OpenModalButton/OpenModalButton";
 import UpdateSaleModal from "../UpdateSaleModal/UpdateSaleModal";
-import Table from "@mui/material/Table";
-import TableContainer from "@mui/material/TableContainer";
+import NewSaleForm from "../NewSaleForm/NewSaleForm"
 import Paper from "@mui/material/Paper";
-import { DataGrid } from "@mui/x-data-grid";
+import Button from "@mui/material/Button";
+import Box from '@mui/material/Box';
+import { DataGrid, GridToolbarContainer } from "@mui/x-data-grid";
 
 function SalesTrackerTable() {
 	const dispatch = useDispatch();
@@ -29,11 +30,36 @@ function SalesTrackerTable() {
 	};
 
 	const columns = [
-		{ field: "accountNumber", headerName: "Account", type: "number", width: 80, align: "right", headerAlign: "right" },
-		{ field: "planType", headerName: "Plan", width: 100, align: "right", headerAlign: "right" },
-		{ field: "initialPrice", headerName: "Initial", type: "number", align: "right", width: 80, headerAlign: "right" },
-		{ field: "recurringPrice", headerName: "Recurring", type: "number", align: "right", width: 100, headerAlign: "right" },
 		{
+			field: "accountNumber",
+			headerName: "Account",
+			type: "number",
+			width: 80,
+			align: "right",
+			headerAlign: "right",
+			resizable: false,
+		},
+		{ field: "planType", headerName: "Plan", width: 100, align: "right", headerAlign: "right", resizable: false },
+		{
+			field: "initialPrice",
+			headerName: "Initial",
+			type: "number",
+			align: "right",
+			width: 80,
+			headerAlign: "right",
+			resizable: false,
+		},
+		{
+			field: "recurringPrice",
+			headerName: "Recurring",
+			type: "number",
+			align: "right",
+			width: 100,
+			headerAlign: "right",
+			resizable: false,
+		},
+		{
+			field: "CV",
 			headerName: "CV",
 			type: "number",
 			width: 70,
@@ -54,14 +80,22 @@ function SalesTrackerTable() {
 						cv = "N/A";
 				}
 				return cv;
-			}
-
+			},
+			resizable: false,
 		},
-		{ field: "agreementLength", headerName: "Length", type: "number", align: "right", width: 80, headerAlign: "right" },
 		{
-			field: "ez",
+			field: "agreementLength",
+			headerName: "Length",
+			type: "number",
+			align: "right",
+			width: 80,
+			headerAlign: "right",
+			resizable: false,
+		},
+		{
+			field: "EZ",
 			headerName: "EZ Pay",
-			width: 60,
+			width: 80,
 			renderCell: (cellValues) => {
 				const { autopay, ach } = cellValues.row;
 				if (autopay && ach) {
@@ -71,7 +105,10 @@ function SalesTrackerTable() {
 				} else {
 					return "None";
 				}
-			}
+			},
+			resizable: false,
+			align: "right",
+			headerAlign: "right",
 		},
 		{
 			field: "initialDate",
@@ -81,33 +118,51 @@ function SalesTrackerTable() {
 			headerAlign: "right",
 			valueFormatter: (params) => {
 				return format(new Date(params), "MM/dd/yy");
-			}
+			},
+			resizable: false,
 		},
 		{ field: "serviced", headerName: "Serviced", width: 100, align: "right", headerAlign: "right" },
-		{
-			field: "updatedAt",
-			headerName: "Last Updated",
-			width: 140,
-			align: 'right',
-			headerAlign: "right",
-			valueFormatter: (params) => {
-				return format(new Date(params), "MM/dd HH:mm");
-			}
+		{ field: "Action", headerName: "Action", width: 100, align: "right", headerAlign: "right" },
 
-		 },
 	];
 
 	const paginationModel = { page: 0, pageSize: 5 };
 
 	return (
 		<>
+			<Box sx={{
+            width: "100%",
+            display: "flex",
+            marginBottom: "10px",
+			paddingTop: "10px",
+			paddingBottom: "0px",
+			paddingLeft: "10px"
+
+        }}>
+            <OpenModalButton
+                buttonText="Add Sale"
+                modalComponent={<NewSaleForm />}
+                cssClass={true}
+                sx={{
+                    backgroundColor: "#1976d2",
+                    color: "white",
+                    "&:hover": {
+                        backgroundColor: "#1565c0" // Darker shade for hover
+                    },
+                    padding: "8px 16px",
+                    borderRadius: "4px",
+                    border: "none",
+                    cursor: "pointer",
+                    fontWeight: "500"
+                }}
+            />
+        </Box>
 			<Paper sx={{ height: 400, width: "100%" }}>
 				<DataGrid
 					rows={sales}
 					columns={columns}
 					initialState={{ pagination: { paginationModel } }}
 					pageSizeOptions={[5, 10]}
-					checkboxSelection
 					sx={{
 						border: 0,
 						"& .MuiDataGrid-columnHeader": {
